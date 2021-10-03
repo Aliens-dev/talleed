56bafaef26b1a20a21fd0bae2723ce4e15930ac2
aliensDev <merazganabil34@gmail.com>
2021-10-03 09:44:25 +0100

    sqlite config

--- config/database.php
+++ config/database.php
@@ -38,7 +38,7 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('taleed.sqlite')),
+            'database' => database_path('taleed.sqlite'),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
